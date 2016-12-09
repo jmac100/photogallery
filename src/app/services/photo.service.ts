5d640e953db4151da3b609fdd0d10407f3e4e136
Jim MacDonald <Jim MacDonald>
2016-12-08 22:54:24 -0500

    upgrade angular-cli to 1.0.0-beta.22-1

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
 @Injectable()
